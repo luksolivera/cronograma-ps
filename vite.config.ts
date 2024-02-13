5f4f172d741814aec77557ff05737df0fa74a07b
Luks <lolivera@andreani.com>
2024-02-13 17:06:21 -0300

    fix: change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'cronograma-ps',
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/material/styles']
